55971bf6a64ddf406b34cb6b896f7b24523a299a
Stephanie Hickman <stephick@gmail.com>
2024-02-21 07:23:31 -0600

    added redux to project and moved data to reducers

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
+import { applyMiddleware, createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux'
+import logger from 'redux-logger';
+import { useState } from 'react';
+
+
+// const [pitcherList, setPitcherList] = useState(['Maud Nelson', 'Ila Borders', 'Don Newcombe', 'CC Sabathia']);
+
+/** Add Reducers **/
+
+const pitcherList = (state = ['Maud Nelson', 'Ila Borders', 'Don Newcombe', 'CC Sabathia'], action) =>{
+
+    if (action.type === "PITCHERLIST_ADD"){
+        console.log(`The pitcher to add is:, ${action.payload}`)
+    
+        return [...state, action.payload]
+    }
+return state;
+}
+
+const catcherList = (state = ['Roy Campanella', 'Elston Howard', 'Kenji Jojima'], action) =>{
+
+    if (action.type === "CATCHERLIST_ADD"){
+        console.log(`The catcher to add is:, ${action.payload}`)
+    
+        return [...state, action.payload]
+    }
+return state;
+}
+
+
+/** Create Store **/
+
+const storeInstance = createStore(
+    combineReducers(
+        {
+            pitcherList,
+            catcherList
+        }
+    ),
+    applyMiddleware(logger)
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store = {storeInstance}>
+            <App />
+        </Provider>
     </React.StrictMode>
 );
